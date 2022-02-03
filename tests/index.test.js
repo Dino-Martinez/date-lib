ed2180bf31c2f159097ea3e835e1f56af363a7a1
Dino-Martinez <Dino.Martinez.514@gmail.com>
2022-02-03 15:30:25 -0500

    Fixed misunderstanding of parameter order

--- tests/index.test.js
+++ tests/index.test.js
@@ -99,9 +99,9 @@ describe('Mutators', () => {
 })
 
 describe('Output', () => {
-  const d = new EasyDate(2022, 22, 2, 8, 30, 0)
+  const d = new EasyDate(2022, 2, 22, 8, 30, 0)
   test('Formatting', () => {
-    expect(d.format('y/n/d')).toEqual('22/22/2')
+    expect(d.format('y/n/d')).toEqual('22/2/22')
     expect(d.format('W M D, Y')).toEqual('Wednesday February 22, 2022')
     expect(d.format('H:I n d')).toEqual('8:30 Feb 22')
   })
